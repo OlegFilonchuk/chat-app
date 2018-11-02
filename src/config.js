a8e6833dde744aaa9d09cbbc122520bbd03d86cd
Oleg Filonchuk <oifilonchuk@gmail.com>
2018-11-02 14:16:48 +0200

    return to startpoint

---
+++ src/config.js
@@ -0,0 +1,6 @@
+const tokenUrl = 'https://us1.pusherplatform.io/services/chatkit_token_provider/v1/d3894417-5357-4418-b0e4-3b9e6973f914/token'
+
+const instanceLocator = 'v1:us1:d3894417-5357-4418-b0e4-3b9e6973f914'
+
+
+export { tokenUrl, instanceLocator }
