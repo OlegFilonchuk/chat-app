d6497891c118904eb9ff4792e01bb6969b43799d
Oleg Filonchuk <oifilonchuk@gmail.com>
2018-11-03 09:44:25 +0200

    add new message form component

---
+++ src/components/NewMessageForm.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+
+class NewMessageForm extends Component {
+  render() {
+    return (
+      <div>
+        New Message Form
+      </div>
+    )
+  }
+}
+
+export default NewMessageForm
