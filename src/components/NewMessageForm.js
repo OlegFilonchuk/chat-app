1708fcc0f3208ec640fa7f505b4062d4471444de
Oleg Filonchuk <oifilonchuk@gmail.com>
2018-11-04 01:12:27 +0200

    add new message form partial functionality

--- src/components/NewMessageForm.js
+++ src/components/NewMessageForm.js
@@ -1,11 +1,42 @@
 import React, {Component} from 'react'
 
 class NewMessageForm extends Component {
+
+  constructor() {
+    super()
+    this.state = {
+      value: ''
+    }
+  }
+
+  handleChange = ev => {
+    this.setState({
+      value: ev.currentTarget.value
+    })
+  }
+
+  handleSubmit = ev => {
+    ev.preventDefault()
+    /*send message*/
+    console.log('---', this.state.value)
+    this.setState({
+      value: ''
+    })
+  }
+
   render() {
     return (
-      <div className="new-message-form">
-        New Message Form
-      </div>
+      <form
+        className="new-message-form"
+        onSubmit={this.handleSubmit}
+      >
+        <input
+          value={this.state.value}
+          onChange={this.handleChange}
+          type="text"
+          placeholder="Type a message and hit ENTER"
+        />
+      </form>
     )
   }
 }
