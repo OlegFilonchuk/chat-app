fdbd4274e5c8219372780f597da55c17d440bc1f
Oleg Filonchuk <oifilonchuk@gmail.com>
2018-11-03 09:45:00 +0200

    add new room form component

---
+++ src/components/NewRoomForm.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+
+class NewRoomForm extends Component {
+  render() {
+    return (
+      <div>
+        New Room Form
+      </div>
+    )
+  }
+}
+
+export default NewRoomForm
